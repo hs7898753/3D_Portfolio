e43dd784d4cb56ccfacdf6f268c06843d0d43490
hs7898753 <hs7898753@gmail.com>
2023-12-30 00:35:55 +0530

    Contact Page Done - using EmailJS and three.js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import Island from '../models/Island'
 import Sky from '../models/Sky'
 import Bird from '../models/Bird'
 import Plane from '../models/Plane'
+import HomeInfo from '../components/HomeInfo'
 
 const Home = () => {
 
@@ -49,6 +50,9 @@ const Home = () => {
 
     return (
         <section className='w-full h-screen relative'>
+            <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+                {currentStage && <HomeInfo currentStage={currentStage} />}
+            </div>
             <Canvas className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
                 camera={{ near: 0.1, far: 1000 }}
             >
@@ -57,17 +61,17 @@ const Home = () => {
                     <ambientLight intensity={0.5} />
                     <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
                     <Bird />
-                    <Sky isRotating={isRotating}/>
+                    <Sky isRotating={isRotating} />
                     <Island position={islandPosition}
                         scale={islandScale}
                         rotation={islandrotation}
                         isRotating={isRotating}
-                        setIsRotating={setIsRotating} 
-                        setCurrentStage={setCurrentStage}/>
-                    <Plane isRotating={isRotating} 
-                    planeScale={planeScale} 
-                    planePosition={planePosition} 
-                    rotation={[0,20,0]}/>
+                        setIsRotating={setIsRotating}
+                        setCurrentStage={setCurrentStage} />
+                    <Plane isRotating={isRotating}
+                        planeScale={planeScale}
+                        planePosition={planePosition}
+                        rotation={[0, 20, 0]} />
                 </Suspense>
             </Canvas>
         </section>
